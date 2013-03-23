531adabd5caf775ecf591c59dc68d5a67191f56b
MF <chungtszhim@gmail.com>
2013-03-23 23:42:47 +0800

    add character, control and game mode

---
+++ game/modules/chat/client/SM_Chat.js
@@ -0,0 +1,17 @@
+﻿var THREE = require('../../../vendor/Three');
+var util = require('../../../framework/Util');
+var GameObjectManager = require('../../../framework/GameObjectManager');
+var World = require('../../../framework/World');
+var io = require('../../../vendor/socket.io-client');
+var ClientMessage = require('../../../framework/net/client/ClientMessage');
+var ServerMessage = require('../../../framework/net/client/ServerMessage');
+
+var SM_Chat = function (data) {
+    console.log(" SM_Chat");
+    console.log(data);
+};
+
+util.inherits(SM_Chat, ServerMessage);
+SM_Chat.NAME = "SM_Login_Response";
+
+module.exports = SM_Chat;
---
+++ game/modules/control/client/PlayerController.js
@@ -0,0 +1,8 @@
+﻿var util = require('../../../framework/Util');
+var AdvancedController = require('../../../framework/controllers/AdvancedController');
+
+var PlayerController = function (data) {
+    this.jumpable = data.jumpable;
+};
+util.inherits(PlayerController, AdvancedController);
+module.exports = PlayerController;

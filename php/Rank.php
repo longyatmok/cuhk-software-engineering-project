03327a72c97caa0776dfb8124e124dd9377e66a2
Allen <a1b2c3d42003hongkong@hotmail.com>
2013-03-24 21:12:44 +0800

    add Rank class (prototype)

---
+++ php/Rank.php
@@ -0,0 +1,12 @@
+<?php
+class Rank{
+	public function getTop10($date){
+		return array(array('nickname'=>'', 'mark'=>'num')); // similar to this but
+	}
+	public function calcul($beforeDate){
+		
+	}
+	public function compare(){
+		
+	}
+}

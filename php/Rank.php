e021da8a0010bc8adc4aec00db9987bb7bc12059
Allen <a1b2c3d42003hongkong@hotmail.com>
2013-03-24 23:57:53 +0800

    Update prototype

--- php/Rank.php
+++ php/Rank.php
@@ -3,10 +3,10 @@ class Rank{
 	public function getTop10($date){
 		return array(array('nickname'=>'', 'mark'=>'num')); // similar to this but
 	}
-	public function calcul($beforeDate){
+	public function compare(){
 		
 	}
-	public function compare(){
+	private function calcul($beforeDate){ // daily function 
 		
 	}
 }
